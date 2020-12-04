f44ea19041864deb814336bb4a0ae34c69fdddb9
Shehroj Khan <shehrojkhan786@gmail.com>
2020-12-04 18:03:48 +0530

    Updaitng for user registration

--- src/main/java/com/hark/configs/WebSecurityConfig.java
+++ src/main/java/com/hark/configs/WebSecurityConfig.java
@@ -63,6 +63,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
+			.antMatchers("/messages**").permitAll()
+			.antMatchers("/chatroom/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
