c95f1c24684c73bafc4d63c9889f5ce54a549dc6
Shehroj Khan <shehrojkhan786@gmail.com>
2021-03-16 16:43:13 +0530

    updating websocket messages url

--- src/main/java/com/hark/configs/WebSecurityConfig.java
+++ src/main/java/com/hark/configs/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.antMatchers("/messages**").permitAll()
-			.antMatchers("/chatroom/**").permitAll()
+			.antMatchers("/discussionRoom/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
