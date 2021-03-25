5a6cd418a32daab15aec0c53ea5c6ead1e0ce133
Shehroj Khan <shehrojkhan786@gmail.com>
2021-03-25 16:54:05 +0530

    Correcting logic for search and create discussion room for user

--- src/main/java/com/hark/controllers/UserController.java
+++ src/main/java/com/hark/controllers/UserController.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.hark.controllers;
 
@@ -38,163 +38,181 @@ import com.hark.services.impl.SearchAndMatchService;
 @RequestMapping("/api/user/")
 public class UserController {
 
-	@Autowired
-	private SearchAndMatchService searchAndMatchService;
+    @Autowired
+    private SearchAndMatchService searchAndMatchService;
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private BadgeRepository badgeRepository;
+    @Autowired
+    private BadgeRepository badgeRepository;
 
-	@Autowired
-	private RatingRepository ratingRepository;
+    @Autowired
+    private RatingRepository ratingRepository;
 
-	@Autowired
-	private DiscussionRepository discussionRepository;
+    @Autowired
+    private DiscussionRepository discussionRepository;
 
-	@Autowired
-	JwtUtils jwtUtils;
+    @Autowired
+    JwtUtils jwtUtils;
 
-	@PostMapping("/searchUserAndCreateRoom")
-	public ResponseEntity<?> searchAndMatch(@Valid @RequestBody String email) {
-		MessageResponse response = new MessageResponse();
-		User user = userRepository.findByEmail(email).get();
-		user.setSearching(true);
-		userRepository.save(user);
-		User opponent = searchAndMatchService.searchUser(user);
-		if (null != opponent) {
-			Discussion room = searchAndMatchService.createDiscussionRoom(user.getId(), opponent.getId());
-			user.setSearching(false);
-			userRepository.save(user);
-			response.setData(room);
-			response.setMessage("Opponents Found");
-			response.setStatus(ResponseStatus.SUCCESS.name());
-			return ResponseEntity.ok(room);
-		}
-		user.setSearching(false);
-		userRepository.save(user);
-		response.setMessage("No opponent found, Try again later");
-		response.setStatus(ResponseStatus.FAILED.name());
-		return ResponseEntity.ok().body(response);
-	}
+    final int MAX_COUNTER = 5;
 
-	@GetMapping("/{username}")
-	public ResponseEntity<?> getUserDetails(@Valid @RequestBody String email) {
-		MessageResponse response = new MessageResponse();
-		User user = null;
-		try {
-			user = userRepository.findByEmail(email).get();
-		} catch (NoSuchElementException ex) {
-			response.setStatus(ResponseStatus.ERROR.name());
-			response.setMessage("Error: Username is invalid");
-			return ResponseEntity.ok(response);
-		}
-		response.setData(user);
-		response.setMessage("Username is valid");
-		response.setStatus(ResponseStatus.SUCCESS.name());
-		return ResponseEntity.ok(response);
-	}
+    @PostMapping("/searchUserAndCreateRoom")
+    public ResponseEntity<?> searchAndMatch(@Valid @RequestBody String email) {
+        MessageResponse response = new MessageResponse();
+        User user = userRepository.findByEmail(email).get();
+        user.setSearching(true);
+        userRepository.save(user);
+        boolean isOpponentFound = false;
+        int searchCounter = 0;
+        Discussion room = null;
+        while (searchCounter < MAX_COUNTER) {
+            User opponent = searchAndMatchService.searchUser(user);
+            if (null != opponent) {
+                room = searchAndMatchService.createDiscussionRoom(user.getId(), opponent.getId());
+                isOpponentFound = true;
+            }
+            if (isOpponentFound) {
+                break;
+            } else {
+				searchCounter++;
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException exception) {
+                    System.out.println("Error while searching for user.");
+                }
+            }
+        }
+        user.setSearching(false);
+        userRepository.save(user);
+        if (isOpponentFound) {
+            response.setData(room);
+            response.setMessage("Opponents Found");
+            response.setStatus(ResponseStatus.SUCCESS.name());
+        } else {
+            response.setMessage("No opponent found, Try again later");
+            response.setStatus(ResponseStatus.FAILED.name());
+        }
+        return ResponseEntity.ok().body(response);
+    }
 
-	@DeleteMapping("/removeRoom")
-	public ResponseEntity<?> removeRoom(@Valid @RequestBody String discussionRoomId) {
-		boolean isDeleted = searchAndMatchService.deleteDiscussionRoom(discussionRoomId);
-		if (isDeleted) {
-			return ResponseEntity.ok(new MessageResponse("Room deleted!!!"));
-		}
-		return ResponseEntity.badRequest().body(new MessageResponse("Cannot delete room, try again later!!!"));
-	}
+    @GetMapping("/{username}")
+    public ResponseEntity<?> getUserDetails(@Valid @RequestBody String email) {
+        MessageResponse response = new MessageResponse();
+        User user = null;
+        try {
+            user = userRepository.findByEmail(email).get();
+        } catch (NoSuchElementException ex) {
+            response.setStatus(ResponseStatus.ERROR.name());
+            response.setMessage("Error: Username is invalid");
+            return ResponseEntity.ok(response);
+        }
+        response.setData(user);
+        response.setMessage("Username is valid");
+        response.setStatus(ResponseStatus.SUCCESS.name());
+        return ResponseEntity.ok(response);
+    }
 
-	@GetMapping("/badges")
-	public ResponseEntity getBadges() {
-		List<Badge> badges = badgeRepository.findAll();
-		return new ResponseEntity(badges, HttpStatus.OK);
-	}
+    @DeleteMapping("/removeRoom")
+    public ResponseEntity<?> removeRoom(@Valid @RequestBody String discussionRoomId) {
+        boolean isDeleted = searchAndMatchService.deleteDiscussionRoom(discussionRoomId);
+        if (isDeleted) {
+            return ResponseEntity.ok(new MessageResponse("Room deleted!!!"));
+        }
+        return ResponseEntity.badRequest().body(new MessageResponse("Cannot delete room, try again later!!!"));
+    }
 
-	@PostMapping("/userRating")
-	public ResponseEntity<?> userRating(@RequestParam("stars") float stars, @RequestParam("toUser") String toUserEmail,
-			@RequestParam("discussionId") String discussionId) {
-		User user = null;
-		Discussion discussion = null;
-		try {
-			user = userRepository.findByEmail(toUserEmail).get();
-		} catch (NoSuchElementException ex) {
-			// log here
-			return ResponseEntity.badRequest().body(new MessageResponse("Invalid username provided: " + toUserEmail));
-		}
+    @GetMapping("/badges")
+    public ResponseEntity getBadges() {
+        List<Badge> badges = badgeRepository.findAll();
+        return new ResponseEntity(badges, HttpStatus.OK);
+    }
 
-		try {
-			discussion = discussionRepository.findById(discussionId).get();
-		} catch (NoSuchElementException ex) {
-			// log here
-			return ResponseEntity.badRequest()
-					.body(new MessageResponse("Invalid chat/discusion id provided: " + discussionId));
-		}
+    @PostMapping("/userRating")
+    public ResponseEntity<?> userRating(@RequestParam("stars") float stars, @RequestParam("toUser") String toUserEmail,
+                                        @RequestParam("discussionId") String discussionId) {
+        User user = null;
+        Discussion discussion = null;
+        try {
+            user = userRepository.findByEmail(toUserEmail).get();
+        } catch (NoSuchElementException ex) {
+            // log here
+            return ResponseEntity.badRequest().body(new MessageResponse("Invalid username provided: " + toUserEmail));
+        }
 
-		UserRating userRating = new UserRating();
-		userRating.setStars(stars);
-		userRating.setChatRoom(discussion);
-		userRating.setUser(user);
-		ratingRepository.save(userRating);
+        try {
+            discussion = discussionRepository.findById(discussionId).get();
+        } catch (NoSuchElementException ex) {
+            // log here
+            return ResponseEntity.badRequest()
+                    .body(new MessageResponse("Invalid chat/discusion id provided: " + discussionId));
+        }
 
-		ExampleMatcher userRatingMatcher = ExampleMatcher.matching().withIgnorePaths("id")
-				.withMatcher("user_id", GenericPropertyMatchers.exact())
-				.withMatcher("discussion_id", GenericPropertyMatchers.exact());
-		Example<UserRating> userRatingExample = Example.of(userRating, userRatingMatcher);
-		boolean isSaved = ratingRepository.exists(userRatingExample);
-		if (isSaved) {
-			return ResponseEntity.ok(new MessageResponse("User rating saved!!!"));
-		}
-		return ResponseEntity.badRequest().body(new MessageResponse("Unable to save ratings please, try again!!!"));
-	}
+        UserRating userRating = new UserRating();
+        userRating.setStars(stars);
+        userRating.setChatRoom(discussion);
+        userRating.setUser(user);
+        ratingRepository.save(userRating);
 
-	@GetMapping("/checkForProfileCompletion")
-	public ResponseEntity<?> isProfileCompleted(@Valid @RequestBody String email) {
-		boolean isProfileCompleted = false;
-		try {
-			isProfileCompleted = userRepository.existsByEmailAndIsProfileCompleted(email, true);
-		} catch (Exception ex) {
-			// log here
-			return ResponseEntity.badRequest()
-					.body(new MessageResponse("Error while checking for profile completion for email: " + email));
-		}
-		if (isProfileCompleted) {
-			return ResponseEntity.ok(new MessageResponse("Profile is completed!!!"));
-		} else {
-			return ResponseEntity.badRequest()
-					.body(new MessageResponse("Profile is not compeleted yet. Please complete profile."));
-		}
-	}
+        ExampleMatcher userRatingMatcher = ExampleMatcher.matching().withIgnorePaths("id")
+                .withMatcher("user_id", GenericPropertyMatchers.exact())
+                .withMatcher("discussion_id", GenericPropertyMatchers.exact());
+        Example<UserRating> userRatingExample = Example.of(userRating, userRatingMatcher);
+        boolean isSaved = ratingRepository.exists(userRatingExample);
+        if (isSaved) {
+            return ResponseEntity.ok(new MessageResponse("User rating saved!!!"));
+        }
+        return ResponseEntity.badRequest().body(new MessageResponse("Unable to save ratings please, try again!!!"));
+    }
 
-	@PostMapping("/saveProfileDetails")
-	public ResponseEntity<?> saveProfileDetails(@Valid @RequestBody Long phone,
-			@Valid @RequestBody String politicalParty, @Valid @RequestBody String country,
-			@Valid @RequestBody String email, @Valid @RequestBody String deviceId) {
-		User user = null;
-		try {
-			user = userRepository.findByEmail(email).get();
-		} catch (NoSuchElementException ex) {
-			// log here
-			return ResponseEntity.badRequest()
-					.body(new MessageResponse("Error while finding user for email: " + email));
-		} catch (Exception ex) {
-			// log here
-			return ResponseEntity.badRequest().body(new MessageResponse("Exception occured: " + ex.getMessage()));
-		}
+    @GetMapping("/checkForProfileCompletion")
+    public ResponseEntity<?> isProfileCompleted(@Valid @RequestBody String email) {
+        boolean isProfileCompleted = false;
+        try {
+            isProfileCompleted = userRepository.existsByEmailAndIsProfileCompleted(email, true);
+        } catch (Exception ex) {
+            // log here
+            return ResponseEntity.badRequest()
+                    .body(new MessageResponse("Error while checking for profile completion for email: " + email));
+        }
+        if (isProfileCompleted) {
+            return ResponseEntity.ok(new MessageResponse("Profile is completed!!!"));
+        } else {
+            return ResponseEntity.badRequest()
+                    .body(new MessageResponse("Profile is not compeleted yet. Please complete profile."));
+        }
+    }
 
-		if (null != user) {
-			try {
-				user.setCountry(country);
-				user.setPoliticalParty(politicalParty);
-				user.setPhone(phone);
-				user.setDeviceId(deviceId);
-				user.setProfileCompleted(true);
-				userRepository.save(user);
-			} catch (Exception ex) {
-				return ResponseEntity.badRequest().body(new MessageResponse("Exception occured: " + ex.getMessage()));
-			}
-		}
-		return ResponseEntity.badRequest().body(new MessageResponse("User not found!!!"));
-	}
+    @PostMapping("/saveProfileDetails")
+    public ResponseEntity<?> saveProfileDetails(@Valid @RequestBody Long phone,
+                                                @Valid @RequestBody String politicalParty, @Valid @RequestBody String country,
+                                                @Valid @RequestBody String email, @Valid @RequestBody String deviceId) {
+        User user = null;
+        try {
+            user = userRepository.findByEmail(email).get();
+        } catch (NoSuchElementException ex) {
+            // log here
+            return ResponseEntity.badRequest()
+                    .body(new MessageResponse("Error while finding user for email: " + email));
+        } catch (Exception ex) {
+            // log here
+            return ResponseEntity.badRequest().body(new MessageResponse("Exception occured: " + ex.getMessage()));
+        }
+
+        if (null != user) {
+            try {
+                user.setCountry(country);
+                user.setPoliticalParty(politicalParty);
+                user.setPhone(phone);
+                user.setDeviceId(deviceId);
+                user.setProfileCompleted(true);
+                userRepository.save(user);
+            } catch (Exception ex) {
+                return ResponseEntity.badRequest().body(new MessageResponse("Exception occured: " + ex.getMessage()));
+            }
+        }
+        return ResponseEntity.badRequest().body(new MessageResponse("User not found!!!"));
+    }
 
 }
