733f72a3e1018e0d95064de587ffe5db87699907
Shehroj Khan <shehrojkhan786@gmail.com>
2021-04-20 23:12:30 +0530

    Upload/downlaod feature completed.

--- src/main/java/com/hark/controllers/MediaController.java
+++ src/main/java/com/hark/controllers/MediaController.java
@@ -3,32 +3,29 @@
  */
 package com.hark.controllers;
 
+import com.hark.model.enums.ResponseStatus;
+import com.hark.model.payload.response.MessageResponse;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.support.HttpRequestHandlerServlet;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.support.HttpRequestHandlerServlet;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.hark.model.payload.response.MessageResponse;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author shkhan
@@ -39,78 +36,119 @@ import com.hark.model.payload.response.MessageResponse;
 @RequestMapping("/api/media")
 public class MediaController {
 
-	private final String fileBasePath = "/files/media/";
+	private final String fileBasePath = "D:/files/media/";
 
-	@PostMapping("/upload")
-	public ResponseEntity<String> uploadData(@RequestParam("file") MultipartFile file,
-			@RequestParam("discussionId") String discussionId, @RequestParam("userId") String userId) throws Exception {
-
+	@PostMapping(value ="/upload/discussions", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<MessageResponse> uploadData(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws Exception {
+		MessageResponse response = new MessageResponse();
+		String discussionRoomId = request.getHeader("discussionroomid");
+		String userId = request.getHeader("userid");
 		if (file == null) {
-			throw new RuntimeException("You must select the a file for uploading");
+			response.setMessage("You must select the a file for uploading");
+			response.setStatus(ResponseStatus.FAILED.name());
+		}else if(StringUtils.isEmpty(userId)){
+			response.setMessage("Kindly, tell me who is uploading the file. UserId is not available in headers.");
+			response.setStatus(ResponseStatus.FAILED.name());
+		}else if(StringUtils.isEmpty(discussionRoomId)){
+			response.setMessage("Kindly, tell me for which discussion you are uploading the file. DiscussionRoomId is not available in headers.");
+			response.setStatus(ResponseStatus.FAILED.name());
+		}else {
+			uploadFile(file, response, "discussions", userId, discussionRoomId);
 		}
-
-		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-		String filePath = String.join("/", fileBasePath, discussionId, userId, fileName);
-		Path path = Paths.get(filePath);
-		try {
-			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path(filePath).toUriString();
-
 		// Do processing with uploaded file data in Service layer
-		return new ResponseEntity<String>(fileDownloadUri, HttpStatus.OK);
+		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping("/download/{filePath:.+}")
-	public ResponseEntity downloadFileFromLocal(@PathVariable String filePath, HttpRequestHandlerServlet request) {
+	@PostMapping(value = "/upload/user", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<MessageResponse> uploadProfileImage(@RequestParam("file") MultipartFile file, HttpServletRequest request){
+		MessageResponse response = new MessageResponse();
+		String userId = request.getHeader("userid");
+		//Optional<String> userId = headers.entrySet().stream().filter(key->"userid".equals(key)).map(Map.Entry::getKey).findFirst();
+		if (file == null) {
+			response.setMessage("You must select the a file for uploading");
+			response.setStatus(ResponseStatus.FAILED.name());
+		}else if(StringUtils.isEmpty(userId) && "null" == userId){
+			response.setMessage("Kindly, tell me who is uploading the file. userId is not available in headers.");
+			response.setStatus(ResponseStatus.FAILED.name());
+		}else {
+			System.out.println("Current userId is: "+userId);
+			uploadFile(file, response, "users",userId,null);
+		}
+		// Do processing with uploaded file data in Service layer
+		return ResponseEntity.ok(response);
+	}
+
+	private void uploadFile(MultipartFile file, MessageResponse response, String uploadFor,String userId, String discussionRoomId) {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		String userPath = userId;
+		if(!StringUtils.isEmpty(discussionRoomId)){
+			userPath = String.join("/",discussionRoomId,userId);
+		}
+
+		String filePath = String.join("/", fileBasePath, uploadFor, userPath, fileName);
+		System.out.println(filePath+" Filepath : userId: "+userId);
+		Path path = Paths.get(filePath);
+		boolean filUploadSuccess = false;
+		try {
+			File directory = new File(path.toUri());
+			if (! directory.exists()){
+				directory.mkdirs();
+			}
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			filUploadSuccess = true;
+		} catch (IOException e) {
+			System.out.println("Error while copying file " + e.getMessage());
+			response.setMessage("Error while copying file. Kindly, try again!!!");
+			response.setStatus(ResponseStatus.ERROR.name());
+		}
+		if (filUploadSuccess) {
+			String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path(filePath).toUriString();
+			response.setMessage("File uploaded successfully.");
+			response.setStatus(ResponseStatus.SUCCESS.name());
+			response.setData(fileDownloadUri.split(fileBasePath)[1]);
+		}
+	}
+
+	@GetMapping(value = "/download/",produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<?> downloadFileFromLocal(@RequestParam("file") String filePath, HttpRequestHandlerServlet request) {
+		MessageResponse response = new MessageResponse();
 		Path path = Paths.get(fileBasePath + filePath);
 		Resource resource = null;
 		MediaType mediaType = null;
 		String contentType = null;
+		boolean isSuccess = false;
 		try {
 			resource = new UrlResource(path.toUri());
 			// Try to determine file's content type
-			contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-
+			//contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+			isSuccess = true;
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
-			return ResponseEntity.badRequest().body(new MessageResponse("Exception occured: "+e.getLocalizedMessage()));
+			response.setMessage("Error: URL is incorrect-> "+e.getMessage());
+			response.setStatus(ResponseStatus.ERROR.name());
 		} catch (IOException e) {
 			e.printStackTrace();
-			return ResponseEntity.badRequest().body(new MessageResponse("Exception occured: "+e.getLocalizedMessage()));
-		} finally {
+			response.setMessage("Error: While Fetching file-> "+e.getMessage()+" \nTry again.");
+			response.setStatus(ResponseStatus.ERROR.name());
+		}catch(Exception e){
+			e.printStackTrace();
+			response.setMessage("Error: While Fetching file-> "+e.getMessage()+" \nTry again.");
+			response.setStatus(ResponseStatus.ERROR.name());
+		}
+
+		if(isSuccess) {
 			// Fallback to the default content type if type could not be determined
 			if (contentType == null) {
 				contentType = "application/octet-stream";
 			}
-		}
+//			response.setMessage("File Found and retrieved.");
+//			response.setStatus(ResponseStatus.SUCCESS.name());
+//			response.setData(resource);
+			return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+					.body(resource);
 
-		return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-				.body(resource);
+		}
+		return  ResponseEntity.ok(response);
 	}
-	
-	@PostMapping("/upload/user")
-	public ResponseEntity<String> uploadProfileImage(@RequestParam("file") MultipartFile file,@RequestParam("userId") String userId) throws Exception {
-
-		if (file == null) {
-			throw new RuntimeException("You must select the a file for uploading");
-		}
-
-		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-		String filePath = String.join("/", fileBasePath, "users", userId, fileName);
-		Path path = Paths.get(filePath);
-		try {
-			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path(filePath).toUriString();
-
-		// Do processing with uploaded file data in Service layer
-		return new ResponseEntity<String>(fileDownloadUri, HttpStatus.OK);
-	}
-
 }
